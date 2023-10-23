7189bc06f351cd1be8e379154e75a017c0b15a5b
Slithery-Snake <71400229+Slithery-Snake@users.noreply.github.com>
2023-10-23 12:50:08 -0400

    Update Network.java

--- src/Network.java
+++ src/Network.java
@@ -9,21 +9,29 @@ import java.lang.Math;
 
 public class Network
 {
-  double[][] hiddenLayers; //Which Layer, which neuron, what weight
+  double[][] hiddenLayers; // which neuron, what weight - first neuron is output
+  double[] neuronValues; //output start
   double[] input;
-  double[] output;
    
   double[] bias;
-    public Network(double[][] hiddenLayers)
+    public Network(double[][] hiddenLayers, double[] input)
     {
         this.hiddenLayers = hiddenLayers;
+        this.input = input;
+        
+        neuronValues = new double[hiddenLayers[0].length-1 + input.length];
     }
     
     private double SigmoidSquish(double input) {
         return 1/(1+ Math.exp(-input));
     }
-    private double addLayer() {
-    
-    return 3.0;
+    private double CalculateNeuron(int index) {
+    double[] weights = hiddenLayers[index];
+    double weightedSum = 0;
+    for(int i = 0; i < weights.length-1; i++) {
+        weightedSum += weights[i]* neuronValues[index*weights.length - i];
+    }
+    weightedSum += weights[weights.length];
+    return SigmoidSquish(weightedSum);
     }
 }
